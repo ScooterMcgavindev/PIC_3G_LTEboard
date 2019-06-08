61848bfcc0d6396e59d633236fa9c2b9d8e1753a
dillon <dmanhall@gmail.com>
2019-06-07 23:13:08 -0600

    updated readme and added a main along with some basic macro options

---
+++ pic_lte_main.c
@@ -0,0 +1,17 @@
+#include "testconfig.h"
+
+#ifdef TESTMODE
+// includes for system
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <assert.h>
+// includes for testing the sparkfun_lte system
+#include "sparkfun_lte.h"
+
+int main()
+{
+    printf("%s\n","running tests...");
+}
+
+#endif
